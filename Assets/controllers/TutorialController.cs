f375db46330f6ac71e8fa9d57dc551a110f043f8
sigurp <skoladoddi@gmail.com>
2017-04-04 19:23:09 +0000

    Added Tutorial To gameplay
    Added simple instructions to gameplay screen that disappear after a
    while.

---
+++ Assets/controllers/TutorialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+    public GameObject tmp;
+
+    public float time = 10; // tíminn sem texti er uppi.
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Hide", time);
+	}
+
+    void Hide()
+    {
+        // Fela tutorial.
+        tmp.SetActive(false);
+    }
+	
+}
